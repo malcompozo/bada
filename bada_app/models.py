7da412c4d2b81e4a31d03f6aed50a3675d9aefc6
malcompozo <malcompozo@gmail.com>
2022-04-28 00:15:36 -0400

    modelo slider

--- bada_app/models.py
+++ bada_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Slider(models.Model):
     title = models.CharField(max_length=100)
-    content = models.EmailField(max_length=100)
+    content = models.CharField(max_length=100)
     image = models.CharField(max_length=200)
 
     def __str__(self):
