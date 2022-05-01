2ac17bc57d959cc0f30cec2c56335ed997969fbb
malcompozo <malcompozo@gmail.com>
2022-05-01 17:47:26 -0400

    Consultar evento

--- badaRest/settings.py
+++ badaRest/settings.py
@@ -140,8 +140,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage' 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
