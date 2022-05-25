9fe12ec93f0aff84ac7fdc93d99d16b6ca3afdb5
malcompozo <malcompozo@gmail.com>
2022-05-24 23:03:14 -0400

    update requirements.txt file

--- badaRest/settings.py
+++ badaRest/settings.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
