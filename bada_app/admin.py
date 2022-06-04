3ac9a6911bd16b222154d1490af2cdd651b471f4
malcompozo <malcompozo@gmail.com>
2022-06-04 12:28:29 -0400

    server error

--- bada_app/admin.py
+++ bada_app/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from bada_app import models
 
 class EventAdmin (admin.ModelAdmin):
-    readonly_fields = ('created', 'updated')
+    readonly_fields = ('search_id', 'created', 'updated')
     list_display = ('state','event_type', 'search_id','created')
     search_fields = ('state', 'event_type')
 
 class CustomerAdmin (admin.ModelAdmin):
-    readonly_fields = ('created',)
+    readonly_fields = ('created', 'event_booking')
     list_display = ('name', 'last_name','created', 'event_booking')
-    search_fields = ('rut', 'email')    
+    search_fields = ('rut', 'email', 'event_booking')    
 
 
 admin.site.register(models.EventBooking, EventAdmin)
